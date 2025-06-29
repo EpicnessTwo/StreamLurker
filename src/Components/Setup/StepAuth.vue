0dff5771bbc473f2b945dd9a6f293f1bbac4a077
EpicKitty <github@epickitty.uk>
2025-06-29 23:11:12 +0100

    Implemented fancy oauth landing page

--- src/Components/Setup/StepAuth.vue
+++ src/Components/Setup/StepAuth.vue
@@ -17,12 +17,15 @@ function getAccessTokenFromUrl(url) {
   return params.get('access_token');
 }
 
+const success_html = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>StreamLurker &bull; Auth Complete</title><meta name="viewport" content="width=device-width, initial-scale=1.0"><style>html, body {margin: 0;padding: 0;background: #0f172a;color: #f1f5f9;font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;height: 100vh;display: flex;align-items: center;justify-content: center;flex-direction: column;text-align: center;}.check {font-size: 4rem;color: #10b981;margin-bottom: 1rem;}.message {font-size: 1.25rem;max-width: 24rem;}.logo {margin-bottom: 1rem;font-size: 1.75rem;font-weight: bold;color: #38bdf8;}</style></head><body><div class="logo">StreamLurker</div><div class="check">✔️</div><div class="message">Twitch authentication complete!<br><br>You can now return to <strong>StreamLurker</strong> and close this window.</div></body></html>`;
+
 async function startTwitchAuthFlow() {
 
   try {
     const port = await start(
         {
-          ports: [8000, 8001, 8002, 8003, 8004, 8005, 8006, 8007, 8008, 8009]
+          ports: [8000, 8001, 8002, 8003, 8004, 8005, 8006, 8007, 8008, 8009],
+          response: success_html,
         }
     );
     console.log(`OAuth server started on port ${port}`);
